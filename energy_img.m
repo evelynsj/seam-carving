dd46f9571ca7009da7b8c807a8b8e323d26f850a
Evelyn Sjafii <evelynsj98@gmail.com>
2019-04-29 18:45:58 -0700

    Use prewitt filter

---
+++ energy_img.m
@@ -0,0 +1,9 @@
+function energyImg = energy_img(im)
+    gray = rgb2gray(im); % convert image to grayscale
+    
+    h = fspecial('prewitt'); % emphasize horizontal, transpose to emphazie vertical
+    dx = imfilter(im2double(gray), h'); % vertical
+    dy = imfilter(im2double(gray), h); % horizontal
+    energyImg = sqrt(dx.^2 + dy.^2);
+
+end
