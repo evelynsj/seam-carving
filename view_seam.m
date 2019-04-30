3fee1b9f12848d88cff7eaf398654c5ad9d7bc44
Evelyn Sjafii <evelynsj98@gmail.com>
2019-04-30 00:37:41 -0700

    Plot vertical seam

--- view_seam.m
+++ view_seam.m
@@ -1,11 +1,17 @@
 function view_seam(im, seam, seamDirection)
     seamPath = flip(seam); % y-axis
     % x-axis: index of vector
+    
+    % for vertical seam, seamPath should be the x-axis while y-axis is 1 to
+    % num cols of seam
     if strcmp(seamDirection, 'HORIZONTAL')
         imagesc(im);
         hold on;
         plot(seamPath, 'Color', 'red');
     else
-        % TODO
+        y_axis = 1:size(seamPath, 2);
+        imagesc(im);
+        hold on;
+        plot(seamPath, y_axis, 'Color', 'red');
     end
 end
