acb32a968bbe7d2819d029b6148b1791658a0656
Evelyn Sjafii <evelynsj98@gmail.com>
2019-04-29 23:34:41 -0700

    added functionality for finding vertical seam

---
+++ find_vertical_seam.m
@@ -0,0 +1,43 @@
+function verticalSeam = find_vertical_seam(cumulativeEnergyMap)
+[rows, cols] = size(cumulativeEnergyMap);
+verticalSeam = zeros(1, rows); 
+ct = 1;
+
+% choose minimum from last row
+[~,idx] = min(cumulativeEnergyMap(rows,:));
+verticalSeam(ct) = idx;
+ct = ct + 1;
+c = idx;
+
+% backtrack
+for r = rows:-1:2
+    if c == 1
+        M = min([cumulativeEnergyMap(r-1,c), cumulativeEnergyMap(r-1,c+1)]);
+        if M == cumulativeEnergyMap(r-1,c)
+            idx = c;
+        else
+            idx = c+1;
+        end
+    elseif c == cols
+        M = min([cumulativeEnergyMap(r-1,c-1), cumulativeEnergyMap(r-1,c)]);
+        if M == cumulativeEnergyMap(r-1,c-1)
+            idx = c-1;
+        else
+            idx = c;
+        end
+    else
+        M = min([cumulativeEnergyMap(r-1,c-1), cumulativeEnergyMap(r-1,c), cumulativeEnergyMap(r-1,c+1)]);
+        if M == cumulativeEnergyMap(r-1,c-1)
+            idx = c-1;
+        elseif M == cumulativeEnergyMap(r-1,c)
+            idx = c;
+        else
+            idx = c+1;
+        end
+    end
+    verticalSeam(ct) = idx;
+    c = idx;
+    ct = ct + 1;
+end
+
+end
