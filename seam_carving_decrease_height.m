b476eeb3c948915a503cfe6db6dbe34eba4f68e3
Evelyn Sjafii <evelynsj98@gmail.com>
2019-04-30 20:09:47 -0700

    changed to decrease by 50px instead

--- seam_carving_decrease_height.m
+++ seam_carving_decrease_height.m
@@ -4,14 +4,14 @@ mall = imread('inputSeamCarvingMall.jpg');
 pragueEnergy = energy_img(prague);
 mallEnergy = energy_img(mall);
 
-% reduce height by 100 px
-for n=1:100
+% reduce height by 50 px
+for n=1:50
     [reducedPragueColor, reducedPragueEnergy] = decrease_height(prague, pragueEnergy);
     prague = reducedPragueColor;
     pragueEnergy = reducedPragueEnergy;
 end
 
-for n=1:100
+for n=1:50
     [reducedMallColor, reducedMallEnergy] = decrease_height(mall, mallEnergy);
     mall = reducedMallColor;
     mallEnergy = reducedMallEnergy;
